18603607620275aea5960238df922a115d8e157e
phemethyst <phemethyst@proton.me>
2026-08-26 00:48:06 +0200

    added a basic pressure counter

--- src/main/java/org/phemethyst/postmortem/mixin/BoilerDataMixin.java
+++ src/main/java/org/phemethyst/postmortem/mixin/BoilerDataMixin.java
@@ -2,15 +2,23 @@ package org.phemethyst.postmortem.mixin;
 
 import com.simibubi.create.AllBlocks;
 import com.simibubi.create.api.stress.BlockStressValues;
+import com.simibubi.create.content.fluids.tank.CreativeFluidTankBlockEntity;
+import com.simibubi.create.content.fluids.tank.FluidTankBlockEntity;
+import com.simibubi.create.content.fluids.tank.SoundPool;
+import com.simibubi.create.foundation.advancement.AllAdvancements;
 import com.simibubi.create.foundation.utility.CreateLang;
+import net.createmod.catnip.animation.LerpedFloat;
 import net.minecraft.ChatFormatting;
+import net.minecraft.core.Direction;
 import net.minecraft.network.chat.CommonComponents;
 import net.minecraft.network.chat.Component;
 import net.minecraft.network.chat.MutableComponent;
+import net.minecraft.world.level.Level;
 import org.spongepowered.asm.mixin.Mixin;
 import com.simibubi.create.content.fluids.tank.BoilerData;
 import org.spongepowered.asm.mixin.Shadow;
 
+import java.util.EnumMap;
 import java.util.List;
 
 @Mixin(BoilerData.class)
@@ -29,6 +37,22 @@ public class BoilerDataMixin  {
     public boolean passiveHeat;
     @Shadow
     private static final int waterSupplyPerLevel = 10;
+    @Shadow
+    static final int SAMPLE_RATE = 5;
+    @Shadow
+    public LerpedFloat gauge = LerpedFloat.linear();
+    @Shadow
+    private final EnumMap<Direction, SoundPool> pools = new EnumMap<>(Direction.class);
+    @Shadow
+    public boolean needsHeatLevelUpdate;
+    @Shadow
+    int ticksUntilNextSample;
+    @Shadow
+    float[] supplyOverTime = new float[10];
+    @Shadow
+    int currentIndex;
+    @Shadow
+    int gatheredSupply;
 
     @Shadow
     public void calcMinMaxForSize(int boilerSize) {}
@@ -54,6 +78,11 @@ public class BoilerDataMixin  {
     }
 
     @Shadow
+    public boolean updateTemperature(FluidTankBlockEntity controller) {
+        throw new AssertionError();
+    }
+
+    @Shadow
     public float getEngineEfficiency(int boilerSize) {
         throw new AssertionError();
     }
@@ -61,6 +90,67 @@ public class BoilerDataMixin  {
     @Shadow
     public boolean isActive() {
         throw new AssertionError();
+    }
+
+    @Shadow
+    private int getActualHeat(int boilerSize) {
+        throw new AssertionError();
+    }
+
+    private int tankSize; // The size of the boiler's tank. Used for calculating maximum pressure.
+
+    private static final int maxPressurePerTank = 80000;
+    private static final int pressurePerHeatLevel = 4;
+
+    private int currentPressure; // The current pressure of the boiler.
+    private int maxPressure; // The maximum pressure of the boiler.
+
+    public void tick(FluidTankBlockEntity controller) {
+        if (!isActive())
+            return;
+
+        tankSize = controller.getTotalTankSize();
+        maxPressure = tankSize * maxPressurePerTank;
+
+        currentPressure = (int) (currentPressure + waterSupply * activeHeat * pressurePerHeatLevel);
+
+        Level level = controller.getLevel();
+        if (level.isClientSide) {
+            pools.values().forEach(p -> p.play(level));
+            gauge.tickChaser();
+            float current = gauge.getValue(1);
+            if (current > 1 && level.random.nextFloat() < 1 / 2f)
+                gauge.setValueNoUpdate(current + Math.min(-(current - 1) * level.random.nextFloat(), 0));
+            return;
+        }
+        if (needsHeatLevelUpdate && updateTemperature(controller))
+            controller.notifyUpdate();
+        ticksUntilNextSample--;
+        if (ticksUntilNextSample > 0)
+            return;
+        int capacity = controller.getTankInventory().getCapacity();
+        if (capacity == 0)
+            return;
+
+        ticksUntilNextSample = SAMPLE_RATE;
+        supplyOverTime[currentIndex] = gatheredSupply / (float) SAMPLE_RATE;
+        waterSupply = Math.max(waterSupply, supplyOverTime[currentIndex]);
+        currentIndex = (currentIndex + 1) % supplyOverTime.length;
+        gatheredSupply = 0;
+
+        if (currentIndex == 0) {
+            waterSupply = 0;
+            for (float i : supplyOverTime)
+                waterSupply = Math.max(i, waterSupply);
+        }
+
+        if (controller instanceof CreativeFluidTankBlockEntity)
+            waterSupply = waterSupplyPerLevel * 20;
+
+        if (getActualHeat(controller.getTotalTankSize()) == 18)
+            controller.award(AllAdvancements.STEAM_ENGINE_MAXED);
+
+        controller.notifyUpdate();
     }
 
     public boolean addToGoggleTooltip(List<Component> tooltip, boolean isPlayerSneaking, int boilerSize) {
@@ -74,6 +164,21 @@ public class BoilerDataMixin  {
         CreateLang.builder().add(getSizeComponent(true, false)).forGoggles(tooltip, 1);
         CreateLang.builder().add(getWaterComponent(true, false)).forGoggles(tooltip, 1);
         CreateLang.builder().add(getHeatComponent(true, false)).forGoggles(tooltip, 1);
+
+        tooltip.add(CommonComponents.EMPTY);
+
+        CreateLang.translate("boiler.pressure")
+                .style(ChatFormatting.LIGHT_PURPLE)
+                .add(CreateLang.text(" "))
+                .add(CreateLang.number(currentPressure))
+                    .style(ChatFormatting.AQUA)
+                .add(CreateLang.text("PU/"))
+                    .style(ChatFormatting.GRAY)
+                .add(CreateLang.number(maxPressure))
+                    .style(ChatFormatting.GRAY)
+                .add(CreateLang.text("PU"))
+                    .style(ChatFormatting.GRAY)
+                .forGoggles(tooltip, 1);
 
         if (attachedEngines == 0)
             return true;
